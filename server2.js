13adc7fb4d8fb4691124ce2dce2fb54c3e211512
Cibele Kawasaki <134068459+CibeleKawasakiNinja@users.noreply.github.com>
2023-05-19 11:40:01 -0300

    Add files via upload

--- server2.js
+++ server2.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const pathToFile = __dirname + '/paises.json';
 
 http.createServer((req, res) => {
-  if (req.url === '/paisescomb') {
+  if (req.url === '/paises') {
     // Define o cabeçalho da resposta para JSON
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -21,46 +21,96 @@ http.createServer((req, res) => {
 
       // Analisa o conteúdo JSON
       const paises = JSON.parse(data);
+      var paisesPrimeiros20 = [];
 
-      // criar um novo array para a lista dos paises com B
-      var paisescomb = [];
+      paises.forEach((element,index) => { // adicione um parametro "index"à função forEach
 
-      paises.forEach(element => {
-
-        var nomeTemporario=element.nome_pais;
-
-        nomeTemporario = String(nomeTemporario);
-
-        var primeiraLetra = nomeTemporario.substr(0,1);
-
-        if(primeiraLetra == "B"){
-
-          paisescomb.push(element.nome_pais);
-
-          console.log(nomeTemporario);
+        if(index < 20 ){ // verifica se o indice é menor que 20
+          paisesPrimeiros20.push(element.nome_pais);
+           
         }
-        
-      });
-       
+});
+      // Gera a resposta HTTP com o conteúdo JSON contendo os primeiros 20 paises
+ res.statusCode = 200;
+ res.end(JSON.stringify(paisesPrimeiros20));
+});
+} 
+else if (req.url === '/paisescomb') {
+  // Define o cabeçalho da resposta para JSON
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Lê o arquivo paises.json
+  fs.readFile(pathToFile, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.statusCode = 500;
+      res.end('Erro interno do servidor');
+      return;
+    }
+// Analisa o conteúdo JSON
+    const paises = JSON.parse(data);
+
+    var paisescomb = [];
+
+    paises.forEach(element => {
+
+      var nomeTemporario=element.nome_pais;
+
+      nomeTemporario = String(nomeTemporario);
+
+      var primeiraLetra = nomeTemporario.substr(0,1);
+
+      if(primeiraLetra == "B"){
+
+        paisescomb.push(element.nome_pais);
+
+        console.log(nomeTemporario);
+      }
+      
+    });
 
       // Gera a resposta HTTP com o conteúdo JSON
-      res.statusCode = 200;
-      res.end(JSON.stringify(paisescomb));
-    });
-  } 
-  
-  
-  
-  
-  else {
-    // Se a URL não for /paises, envia o arquivo index.html
-    res.writeHead(200, {
-      'Content-Type': 'text/html',
-      'Access-Control-Allow-Origin' : '*'
-    });
-    let readStream = fs.createReadStream(__dirname + '/index.html');
-    readStream.pipe(res);
-  }
+ res.statusCode = 200;
+ res.end(JSON.stringify(paisescomb));
+});
+}  
+
+else if (req.url === '/siglas') {
+  // Define o cabeçalho da resposta para JSON
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+// Lê o arquivo paises.json
+  fs.readFile(pathToFile, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.statusCode = 500;
+      res.end('Erro interno do servidor');
+      return;
+    }
+    // Analisa o conteúdo JSON
+    const paises = JSON.parse(data);
+
+    var siglas = [];
+
+    paises.forEach(element => {
+      siglas.push(element.sigla);
+    })
+    // Gera a resposta HTTP com o conteúdo JSON contendo apenas as siglas
+ res.statusCode = 200;
+ res.end(JSON.stringify(siglas));
+});
+}  
+else {
+  // Se a URL não for /paises, envia o arquivo index.html
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+    'Access-Control-Allow-Origin' : '*'
+  });
+  let readStream = fs.createReadStream(__dirname + '/index.html');
+  readStream.pipe(res);
+}
 }).listen(8000);
 
 console.log('Visite-me em: http://localhost:8000');
